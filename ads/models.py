6b5fbbb3e41fe95b1102a51569aa7ce1b39bce0a
Paul <paul.alex.pax@gmail.com>
2018-04-24 17:47:08 +0530

    Add properties to ad model

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Ad(models.Model):
+    advertiser = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date ad end')
+    play_count = models.IntegerField()
+
 # Create your models here.
