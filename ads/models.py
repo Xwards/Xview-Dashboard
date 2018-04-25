76c184c15c3044dfec8b95fee58673e9325bf839
Paul Alex <paul.alex.pax@gmail.com>
2018-04-25 16:53:55 +0530

    Add few more props to Ad model

--- ads/models.py
+++ ads/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 class Ad(models.Model):
-    advertiser = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('date ad end')
-    play_count = models.IntegerField(default=0)
+    advertiser = models.CharField(max_length=2000)
+    email_id = models.EmailField()
+    name = models.CharField(max_length=2000)
+    video = models.CharField(max_length=2000)
+    banner = models.CharField(max_length=200)
+    start_date = models.DateTimeField('Date Ad Starts')
+    end_date = models.DateTimeField('Date Ad Ends')
+    play_count = models.IntegerField(default=0, editable=False)
+    touch_count = models.IntegerField(default=0, editable=False)
+    play_duration = models.DurationField(editable=False)
+
 
     def __str__(self):
         "Set to display advertiser name in the ads table"
