2a97d02ac5d8664c644a076d5d2623ee859342b4
Paul <paul.alex.pax@gmail.com>
2018-04-25 12:16:49 +0530

    Set play count default to zero
    Fixes #1

--- ads/models.py
+++ ads/models.py
@@ -4,6 +4,6 @@ class Ad(models.Model):
     advertiser = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     end_date = models.DateTimeField('date ad end')
-    play_count = models.IntegerField()
+    play_count = models.IntegerField(default=0)
 
 # Create your models here.
